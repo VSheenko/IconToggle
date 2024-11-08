fd39f0be2d6b4de64df71016dd27ef84eb89026e
VSheenko <vovan072005@gmail.com>
2024-11-08 23:00:59 +0300

    Добавил иконку в трей
    На вызове меню настроек пока стоит заглушка
    
    ~ Изменил немного дизайн меню настроек

--- sources/IconToggle.cpp
+++ sources/IconToggle.cpp
@@ -1,10 +1,14 @@
 #include "IconToggle.h"
+#include "TrayHeader.h"
 
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    HWND hwnd = InitInstance(hInstance);
+    InitTrayWnd(hwnd);
+
     hHook = SetWindowsHookEx(WH_MOUSE_LL,
-                          MouseHookProc,
-                          nullptr,
-                          0);
+                             MouseHookProc,
+                             nullptr,
+                             0);
 
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0)) {
