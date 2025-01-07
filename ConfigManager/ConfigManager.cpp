ffff241d6c5d631322f1079253343c151f61cdfd
VSheenko <vovan072005@gmail.com>
2025-01-08 00:50:32 +0300

    Работа с конфиг файлом

---
+++ ConfigManager/ConfigManager.cpp
@@ -0,0 +1,49 @@
+#include "ConfigManager.h"
+
+std::unordered_map<std::string, std::shared_ptr<ConfigManager>> ConfigManager::instances;
+
+bool ConfigManager::Serialization() {
+    std::ofstream file(configPath, std::ios::binary);
+    if (!file.is_open())
+        return false;
+
+    file.write(reinterpret_cast<char*>(&keys), sizeof(keys));
+    file.write(reinterpret_cast<char*>(&autoHideTime), sizeof(autoHideTime));
+    file.write(reinterpret_cast<char*>(&isLBM), sizeof(isLBM));
+    file.write(reinterpret_cast<char*>(&isShortcut), sizeof(isShortcut));
+
+    return true;
+}
+
+bool ConfigManager::Deserialization() {
+    std::ifstream file(configPath, std::ios::binary);
+    if (!file.is_open())
+        return false;
+
+    file.read(reinterpret_cast<char*>(&keys), sizeof(keys));
+    file.read(reinterpret_cast<char*>(&autoHideTime), sizeof(autoHideTime));
+    file.read(reinterpret_cast<char*>(&isLBM), sizeof(isLBM));
+    file.read(reinterpret_cast<char*>(&isShortcut), sizeof(isShortcut));
+
+    return true;
+}
+
+
+std::shared_ptr<ConfigManager> ConfigManager::Instance(const std::string &configPath) {
+    if (!FileExist(configPath))
+        return nullptr;
+
+    if (instances.find(configPath) != instances.end())
+        return instances[configPath];
+
+    instances[configPath] = std::make_shared<ConfigManager>(configPath);
+    return instances[configPath];
+}
+
+ConfigManager::ConfigManager(const std::string &configPath) : configPath(configPath) {}
+
+bool ConfigManager::FileExist(const std::string &path) {
+    DWORD fileAttributes = GetFileAttributesA(path.c_str());
+    return (fileAttributes != INVALID_FILE_ATTRIBUTES &&
+            !(fileAttributes & FILE_ATTRIBUTE_DIRECTORY));
+}
---
+++ ConfigManager/ConfigManager.h
@@ -0,0 +1,33 @@
+#ifndef ICONTOGGLE_CONFIGMANAGER_H
+#define ICONTOGGLE_CONFIGMANAGER_H
+
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <unordered_map>
+
+class ConfigManager {
+private:
+    static std::unordered_map<std::string, std::shared_ptr<ConfigManager>> instances;
+    std::string configPath;
+
+public:
+    unsigned char keys[3];
+    unsigned short autoHideTime;
+    bool isLBM;
+    bool isShortcut;
+
+public:
+    static bool FileExist(const std::string& path);
+    explicit ConfigManager(const std::string& configPath);
+    static std::shared_ptr<ConfigManager> Instance(const std::string& configPath);
+
+    bool Serialization();
+    bool Deserialization();
+};
+
+
+
+
+#endif //ICONTOGGLE_CONFIGMANAGER_H
